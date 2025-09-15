7dc29a9176835ef2d573d978e23e5d26b8e35636
verromaniuk <ver.romaniuk@gmail.com>
2025-09-15 18:55:54 +0300

    debug line added again

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+console.log('BASE_URL =', import.meta.env.BASE_URL);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
